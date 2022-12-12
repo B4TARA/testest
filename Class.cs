19aff007af92d6ffea80021640ff621383a39ce0
B4TARA <70806762+B4TARA@users.noreply.github.com>
2022-12-12 21:50:41 +0300

    Add files via upload

--- Class.cs
+++ Class.cs
@@ -6,18 +6,18 @@
         public int service_number {get;set;}
         public string fullname { get; set; }
         public string position_name { get; set; }
-        public DateOnly position_date { get; set; }
-        public DateOnly hire_date { get; set; }
-        public DateOnly dismiss_date { get; set; }
+        public DateTime position_date { get; set; }
+        public DateTime hire_date { get; set; }
+        public DateTime dismiss_date { get; set; }
         public string department { get; set; }
         public string division_name { get; set; }
         public string sector_name { get; set; }
         public string status { get; set; }
         public string workday_balance { get; set; }
         public string list_number { get; set; }
-        public DateOnly start_date { get; set; }
+        public DateTime start_date { get; set; }
         public string password { get; set; }
-        public DateOnly end_date { get; set; }
+        public DateTime end_date { get; set; }
         public int user_role { get; set; }
         public string email { get; set; }
     }
--- HomeController.cs
+++ HomeController.cs
@@ -41,17 +41,69 @@ namespace Bank.Controllers
             //парсинг в БД//
             WorkBook workbook = WorkBook.Load(filePath);
             WorkSheet sheet = workbook.DefaultWorkSheet;
-            int j = 0;
+            int j = 2;
             foreach (var row in sheet.Rows) //для каждой строки 
             {
                 int i = 0;
+                Class temp = new Class();
                 foreach(var column in sheet.Columns) //для каждого столбца
                 {
-                    Console.Write(sheet.GetCellAt(j, i).Value + " | ");
+                    if(!column.Hidden) //если не скрытый
+                    {
+                        switch (i)
+                        {
+                            case 2: temp.service_number = Convert.ToInt32(sheet.GetCellAt(j, i).Value); break;
+                            case 3: temp.fullname = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 6: temp.position_name = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 7: temp.position_date = Convert.ToDateTime(sheet.GetCellAt(j, i).Value); break;
+                            case 12: temp.hire_date = Convert.ToDateTime(sheet.GetCellAt(j, i).Value); break;
+                            case 22: temp.department = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 24: temp.division_name = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 26: temp.sector_name = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 27: temp.status = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 28: temp.workday_balance = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 30: temp.list_number = Convert.ToString(sheet.GetCellAt(j, i).Value); break;
+                            case 57: temp.start_date = Convert.ToDateTime(sheet.GetCellAt(j, i).Value); break;
+                            case 58: temp.end_date = Convert.ToDateTime(sheet.GetCellAt(j, i).Value); break;
+                        }
+                        //дозаполнить все поля Class
+                        temp.password = "";
+                        temp.email = "";
+                    }
                     i++;
                 }
-                Console.WriteLine();
+                if (j== 3)break;
                 j++;
+                string connString = "Server=localhost; Database=postgres; User Id = postgres; Password = 12345";
+                NpgsqlConnection nc = new NpgsqlConnection(connString);
+                nc.Open();
+                using (nc)
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand("call insert_data_in_Class" + "(:service_number, :fullname, :position_name," +
+                        " :position_date, :hire_date, :dismiss_date, :department, :division_name, :sector_name, :status," +
+                        " :workday_balance, :list_number, :start_date, :pass, :end_date, :user_role, :email)", nc);
+                    cmd.Parameters.AddWithValue("service_number", DbType.Int32).Value = temp.service_number;
+                    cmd.Parameters.AddWithValue("fullname", DbType.String).Value = temp.fullname;
+                    cmd.Parameters.AddWithValue("position_name", DbType.String).Value = temp.position_name;
+                    cmd.Parameters.AddWithValue("position_date", DbType.Date).Value = temp.position_date;
+                    cmd.Parameters.AddWithValue("hire_date", DbType.Date).Value = temp.hire_date;
+                    cmd.Parameters.AddWithValue("dismiss_date", DbType.Date).Value = temp.dismiss_date;
+                    cmd.Parameters.AddWithValue("department", DbType.String).Value = temp.department;
+                    cmd.Parameters.AddWithValue("division_name", DbType.String).Value = temp.division_name;
+                    cmd.Parameters.AddWithValue("sector_name", DbType.String).Value = temp.sector_name;
+                    cmd.Parameters.AddWithValue("status", DbType.String).Value = temp.status;
+                    cmd.Parameters.AddWithValue("workday_balance", DbType.String).Value = temp.workday_balance;
+                    cmd.Parameters.AddWithValue("list_number", DbType.String).Value = temp.list_number;
+                    cmd.Parameters.AddWithValue("start_date", DbType.Date).Value = temp.start_date;
+                    cmd.Parameters.AddWithValue("pass", DbType.String).Value = temp.password;
+                    cmd.Parameters.AddWithValue("end_date", DbType.Date).Value = temp.end_date;
+                    cmd.Parameters.AddWithValue("user_role", DbType.Int32).Value = temp.user_role;
+                    cmd.Parameters.AddWithValue("email", DbType.String).Value = temp.email;
+
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    nc.Close();
+                }
             }
 
             
@@ -82,18 +134,18 @@ namespace Bank.Controllers
                         temp.service_number = Convert.ToInt32(reader[i]); //reader ????
                         temp.fullname = Convert.ToString(reader[i]);
                         temp.position_name = Convert.ToString(reader[i]);
-                        temp.position_date = DateOnly.Parse((string)reader[i]);
-                        temp.hire_date = DateOnly.Parse((string)reader[i]);
-                        temp.dismiss_date = DateOnly.Parse((string)reader[i]);
+                        temp.position_date = (DateTime)reader[i];
+                        temp.hire_date = (DateTime)reader[i];
+                        temp.dismiss_date = (DateTime)reader[i];
                         temp.department = Convert.ToString(reader[i]);
                         temp.division_name = Convert.ToString(reader[i]);
                         temp.sector_name = Convert.ToString(reader[i]);
                         temp.status = Convert.ToString(reader[i]);
                         temp.workday_balance = Convert.ToString(reader[i]);
                         temp.list_number = Convert.ToString(reader[i]);
-                        temp.start_date = DateOnly.Parse((string)reader[i]);
+                        temp.start_date = (DateTime)reader[i];
                         temp.password = Convert.ToString(reader[i]);
-                        temp.end_date = DateOnly.Parse((string)reader[i]);
+                        temp.end_date = (DateTime)reader[i];
                         temp.user_role = Convert.ToInt32(reader[i]);
                         temp.email = Convert.ToString(reader[i]);
                         listtemp.Add(temp);
